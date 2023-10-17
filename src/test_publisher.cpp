723e055818e50a669d3409f6809a5e1326552899
Eunmin Hwang <yesming.em@gmail.com>
2023-10-18 00:30:19 +0900

    polygon test

--- src/test_publisher.cpp
+++ src/test_publisher.cpp
@@ -13,6 +13,9 @@
 #include <memory>
 #include <string>
 
+// #include "geometry_msgs/msg/polygon_stamped.hpp"
+#include <geometry_msgs/msg/polygon_stamped.hpp>
+
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
 
@@ -24,8 +27,11 @@ using namespace std::chrono_literals;
 class MinimalPublisher : public rclcpp::Node {
 public:
   MinimalPublisher() : Node("gaja_visualizer"), count_(0) {
-    publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
+    publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 1);
     timer_ = this->create_wall_timer(500ms, std::bind(&MinimalPublisher::timer_callback, this));
+    pub_ego_vehicle_ = this->create_publisher<geometry_msgs::msg::PolygonStamped>("ego_vehicle", 1);
+    timer_pub_ = this->create_wall_timer(500ms, std::bind(&MinimalPublisher::ego_vehicle_callback, this));
+    ;
   }
 
 private:
@@ -35,8 +41,24 @@ private:
     RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
     publisher_->publish(message);
   }
+  void ego_vehicle_callback() {
+    auto msg = geometry_msgs::msg::PolygonStamped();
+    msg.header.frame_id = "map";
+    auto size = msg.polygon.points.size();
+    for (size_t i = 0; i < 5; ++i) {
+      geometry_msgs::msg::Point32 temp;
+      temp.x = i;
+      temp.y = i;
+      msg.polygon.points.push_back(temp);
+    }
+    RCLCPP_INFO(this->get_logger(), "Publishing: '%d'", msg.polygon.points.size());
+
+    pub_ego_vehicle_->publish(msg);
+  }
   rclcpp::TimerBase::SharedPtr timer_;
+  rclcpp::TimerBase::SharedPtr timer_pub_;
   rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+  rclcpp::Publisher<geometry_msgs::msg::PolygonStamped>::SharedPtr pub_ego_vehicle_;
   size_t count_;
 };
 
