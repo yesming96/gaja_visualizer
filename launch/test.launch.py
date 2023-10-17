df74048102754db640095eac8dd4a44659da1861
Eunmin Hwang <yesming.em@gmail.com>
2023-10-17 23:04:42 +0900

    test pub sub

---
+++ launch/test.launch.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+  # param_dir = LaunchConfiguration(
+  #     'param_dir',
+  #     default=os.path.join(
+  #         get_package_share_directory('gaja_visualizer'),
+  #         'param',
+  #         'arithmetic_config.yaml'))
+  ld = LaunchDescription();
+
+  talk = Node(
+    package='gaja_visualizer',
+    executable='talker',
+    name='talker',
+    parameters=[""])
+
+  listen = Node(
+    package='gaja_visualizer',
+    executable='listener',
+    name='listener',
+    parameters=[""])
+  
+  ld.add_action(talk)
+  ld.add_action(listen)
+  return ld;
